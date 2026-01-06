8a36f849e1c685800842e8b8f93c5c456cba5f02
Gallardo Tristan <157487351+Tristan-Gallardo@users.noreply.github.com>
2026-01-06 17:31:29 +0100

    spécification added

--- src/sgf.ads
+++ src/sgf.ads
@@ -1,5 +1,30 @@
 package sgf is
 
-   
+    type T_SGF is limited private;
+    
+private
+    
+    type T_Noeud is
+        record
+            Name : String;
+            Size: Integer;
+            isDirectory : Boolean;
+            Child : T_Pointer_Children;
+            Parent : T_Pointer_Noeud;
+        end record;
+    type T_Pointer_Noeud is access T_Noeud;
+    
+    type T_Children is 
+        record
+            next : T_Pointer_Noeud;
+            before : T_Pointer_Noeud;
+        end record
+    type T_Pointer_Children is access T_Children;
+    
+    type T_SGF is
+        record
+            Root : T_Pointer_Noeud;
+            Current : T_Pointer_Noeud;
+        end record;
 
 end sgf;
